200f548344fe21eb592f841eefcadbc1fec5ea87
DenooThomas <denoo.thomas@gmail.com>
2022-10-16 14:47:17 +0200

    webshop setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {colors} from "./assets/colors"
+import {createUseStyles} from 'react-jss'
+import Home from "pages/Home/Home"
+const useStyles = createUseStyles({
+  AppCont: {
+    backgroundColor: colors.black,
+    color: 'white'
+  }
+})
 function App() {
+  const isDark = true
+
+  const classes = useStyles()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isDark ? classes.AppCont : 'light'}>
+      <p>test</p>
+      <Home />
     </div>
   );
 }
